38f349225059838b295430c7d79876dfcb6ef539
AbdirahmanMoahemd <kaah6978@gmail.com>
2023-11-27 12:17:54 +0300

    test

--- index.js
+++ index.js
@@ -1,26 +1,61 @@
-import express from 'express';
-import multer from 'multer';
-import path from 'path';
+import express from "express";
+import multer from "multer";
+import path from "path";
 
 const app = express();
 const port = 3000;
 
 // Set up multer storage
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'uploads/'); // Specify the destination folder
+//   },
+//   filename: function (req, file, cb) {
+//     const ext = path.extname(file.originalname);
+//     cb(null, Date.now() + ext); // Set the file name to be unique
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+// // Handle POST request to /upload
+// app.post('/upload', upload.single('image'), (req, res) => {
+//   res.status(200).send('Image uploaded successfully');
+// });
+
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Specify the destination folder
+  destination(req, file, cb) {
+    cb(null, "uploads/");
   },
-  filename: function (req, file, cb) {
-    const ext = path.extname(file.originalname);
-    cb(null, Date.now() + ext); // Set the file name to be unique
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
   },
 });
 
-const upload = multer({ storage: storage });
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
 
-// Handle POST request to /upload
-app.post('/upload', upload.single('image'), (req, res) => {
-  res.status(200).send('Image uploaded successfully');
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb("Images only!");
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  res.send(`/${req.file.path}`);
 });
 
 app.listen(port, () => {
